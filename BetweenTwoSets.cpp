f3abd4c45199a7d253271b121be543120a3ad5b3
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-09-27 10:05:47 +0900

    Update BetweenTwoSets.cpp

--- BetweenTwoSets.cpp
+++ BetweenTwoSets.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
 int gcd(int a, int b)
 {
     return b ? gcd(b, a % b) : a;
